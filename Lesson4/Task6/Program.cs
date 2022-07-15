73c5702f0d8b28fbae03a89b46b1fa5472e76179
mtgstudio <mtg2006@mail.ru>
2022-07-15 18:39:13 +0500

    Решение задачи 6

--- Lesson4/Task6/Program.cs
+++ Lesson4/Task6/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите число: ");
+int x = Convert.ToInt32(Console.ReadLine());
+
+if (x % 2 == 0)
+{
+    Console.WriteLine("Число является четным");
+}
+else
+{
+    Console.WriteLine("Число является нечетным");
+}
