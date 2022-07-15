c45fadadfb7c60c541c56c8aea89de9e089d9c82
mtgstudio <mtg2006@mail.ru>
2022-07-15 19:03:42 +0500

    Доработка решения

--- Lesson4/Task8/Program.cs
+++ Lesson4/Task8/Program.cs
@@ -1,10 +1,10 @@
 ﻿Console.Write("Введите число N: ");
 int N = Convert.ToInt32(Console.ReadLine());
-int M = 1; //Число, от которого идет просмотр
+int M = 1;
 
 if (M % 2 == 0)
 {
-    M = 1;
+    M = M + 0;
 }
 else
 {
@@ -16,4 +16,4 @@ while (M <= N)
     Console.Write(M + " ");
     M = M + 2;
 }
-
+Console.WriteLine();
