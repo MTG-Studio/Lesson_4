9c6c6f8126cb7d3d2bcae9e27d24b1d8feefb1d4
mtgstudio <mtg2006@mail.ru>
2022-07-15 18:34:55 +0500

    Решение задачи 4

--- Lesson4/Task4/Program.cs
+++ Lesson4/Task4/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите первое число: ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int y = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье число: ");
+int z = Convert.ToInt32(Console.ReadLine());
+int max;
+
+if (x > y)
+{
+    if (x > z)
+    {
+        max = x;
+    }
+    else
+    {
+        max = z;
+    }
+}
+else
+    if (y > z)
+    {
+        max = y;
+    }
+    else
+    {
+        max = z;
+    }
+
+Console.WriteLine("Максимальное число равно: " + max);
