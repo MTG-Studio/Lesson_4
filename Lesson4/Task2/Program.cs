f4f9a4b4c82208bd108cbb63f928435cab96d605
mtgstudio <mtg2006@mail.ru>
2022-07-15 18:04:37 +0500

    Решение задачи 2

---
+++ Lesson4/Task2/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите первое число: ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int y = Convert.ToInt32(Console.ReadLine());
+
+if (x > y)
+{
+    Console.WriteLine("Наибольшее число: " + x);
+}
+else
+    Console.WriteLine("Наибольшее число: " + y);
