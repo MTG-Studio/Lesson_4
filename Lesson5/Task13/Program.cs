6d693fc424ce2a0f1f2bf8bab7a24aa9e939919d
mtgstudio <mtg2006@mail.ru>
2022-07-18 22:10:44 +0500

    Решения задач для проверки

---
+++ Lesson5/Task13/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.Write("Введите число: ");
+int x = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+
+int l = x.ToString().Length;    // длина числа
+
+
+if (l > 2)
+{
+    int a = Convert.ToInt32(Math.Pow(10, l - 2));   // для поиска остатка
+    int b = Convert.ToInt32(Math.Pow(10, l - 3));   // для поиска частного
+    int x3 = x % a / b;
+
+    Console.WriteLine(x3);
+}
+else
+{
+    Console.WriteLine("Третьей цифры нет");
+}
