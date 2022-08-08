735c557f3067eb2aecbf82f254e62f6e66a154b0
mtgstudio <mtg2006@mail.ru>
2022-08-08 18:54:32 +0500

    решение ДЗ

---
+++ Lesson7/Task25/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 25: Напишите метод, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+void Pow(double x, double y)
+{ 
+    double result = Math.Pow(x, y);
+    Console.WriteLine($"A в степени B равно {result}");
+}
+Console.Write("Введите число A: ");
+double A = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число B: ");
+double B = Convert.ToInt32(Console.ReadLine());
+Pow(A, B);
---
+++ Lesson7/Task29/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 29: Напишите метод(ы), который задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+void Array()
+{
+    Console.Write("Введите количество элементов массива: ");
+    int q = Convert.ToInt32(Console.ReadLine());
+    string? n;
+    string?[] arr = new string[q];
+    for (int i = 1; i <= q; i++)
+    {
+        Console.Write($"Введите {i}-й элемент массива: ");
+        n = Console.ReadLine();
+        arr[i - 1] = n;
+    }
+    Console.WriteLine("[{0}]", string.Join(", ", arr));
+}
+Array();
