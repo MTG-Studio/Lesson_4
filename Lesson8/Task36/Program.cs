ec5759174765972153fa00811172e9374be17473
mtgstudio <mtg2006@mail.ru>
2022-08-11 21:45:23 +0500

    Решение ДЗ

---
+++ Lesson8/Task36/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+void GetSumEvenPos(int[] arr, int quan)
+{
+    int sumEvenPos = 0;
+    for (int i = 1; i < quan; i += 2)
+        sumEvenPos += arr[i];
+
+    Console.WriteLine(sumEvenPos);
+}
+
+Console.Write("Введите количество элементов массива: ");
+int quantity = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[quantity];
+Random rand = new Random();
+for (int i = 0; i < quantity; i++)
+    array[i] = rand.Next(-100, 100);
+Console.WriteLine("[{0}]", string.Join(", ", array));
+
+GetSumEvenPos(array, quantity);
---
+++ Lesson8/Task38/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 38: Задайте массив вещественных чисел.
+// Найдите разницу между максимальным и минимальным элементов массива.
+// [3 7 22 2 78] -> 76
+
+void GetMaxMinDif(int[] arr, int quan)
+{
+    int max = arr[0];
+    int min = arr[0];
+    for (int i = 0; i < (quan - 1); i++)
+    {
+        if (max < arr[i + 1]) max = arr[i + 1];
+        if (min > arr[i + 1]) min = arr[i + 1];
+    }
+    Console.WriteLine($"max = {max}, min = {min}, dif = {max - min}");
+}
+
+Console.Write("Введите количество элементов массива: ");
+int quantity = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[quantity];
+Random rand = new Random();
+for (int i = 0; i < quantity; i++)
+    array[i] = rand.Next(0, 100);
+Console.WriteLine("[{0}]", string.Join(", ", array));
+
+GetMaxMinDif(array, quantity);
