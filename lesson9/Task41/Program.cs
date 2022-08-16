1f036a75b13acdf989972e5a9bc9fc3ffee56f2f
mtgstudio <mtg2006@mail.ru>
2022-08-16 19:06:46 +0500

    решение ДЗ

---
+++ lesson9/Task41/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+int[] GetArray()
+{
+    Console.Write("Введите количество чисел в массиве: ");
+    int[] array = new int[Convert.ToInt32(Console.ReadLine())];
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"Введите {i + 1}-е число: ");
+        array[i] = Convert.ToInt32(Console.ReadLine());        
+    }
+    return array;
+}
+
+int CountPositive(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+        if (array[i] > 0) count++;
+    return count;
+}
+
+int[] arr = GetArray();
+int countPositive = CountPositive(arr);
+Console.WriteLine($"Массив введенных чисел: {string.Join(", ", arr)}");
+Console.WriteLine($"Количество чисел больше 0: {countPositive}");
---
+++ lesson9/Task43/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых,
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2;
+// значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; 5,5)
+
+// Решение методом Крамера
+// k1 * x - y + b1 = 0;  =>  k1 * x - y = -b1;
+// k2 * x - y + b2 = 0;  =>  k2 * x - y = -b2;
+
+void IntersectionPoint(double c1, double a1, double c2, double a2)
+{
+    double delta = a1 * (-1) - (-1) * a2;
+    double deltax = -c1 * (-1) - (-c2) * (-1);
+    double deltay = a1 * (-c2) - (-c1) * a2;
+
+    if (delta == 0) Console.WriteLine("Точек пересечения нет или их бесконечное количество");
+    else
+    {
+        double x = deltax / delta;
+        double y = deltay / delta;
+        Console.WriteLine($"x = {x}, y = {y}");
+    }
+}
+
+Console.Write("Введите значение b1: ");
+double b1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите значение k1: ");
+double k1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите значение b2: ");
+double b2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите значение k2: ");
+double k2 = Convert.ToDouble(Console.ReadLine());
+
+IntersectionPoint(b1, k1, b2, k2);
