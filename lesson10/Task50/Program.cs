c028baec1ee159595f0445c71431bbfc72252afe
mtgstudio <mtg2006@mail.ru>
2022-08-19 19:10:17 +0500

    Выполнение ДЗ 47 50 52 и **

---
+++ lesson10/Task50/Program.cs
@@ -0,0 +1,54 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве,
+// и возвращает значение этого элемента или же указание, что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 1,7 -> такого числа в массиве нет
+// задача посложнее - вводить позицию в одну строку. символ-разделитель - запятая
+
+int[,] GetMatrix()
+{
+    Console.Write($"Введите количество строк двумерного массива: ");
+    int rows = Convert.ToInt32(Console.ReadLine());
+    Console.Write($"Введите количество столбцов двумерного массива: ");
+    int cols = Convert.ToInt32(Console.ReadLine());
+    int[,] matrix = new int[rows, cols];
+    Random rand = new Random();
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            matrix[i, j] = rand.Next(-100, 100);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]}  ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void ShowMatrixElement(int[,] matrix)
+{
+    Console.Write("Введите через запятую позицию элемента массива: ");
+    string? text = Console.ReadLine();
+    string[] position = text!.Split(new char[] { ',' });
+    int i = Convert.ToInt32(position[0]) - 1;
+    int j = Convert.ToInt32(position[1]) - 1;
+    if (i < matrix.GetLength(0) && j < matrix.GetLength(1))
+        Console.WriteLine(matrix[i, j]);
+    else Console.WriteLine("Такого числа в массиве нет");
+}
+
+int[,] matrix1 = GetMatrix();
+PrintMatrix(matrix1);
+ShowMatrixElement(matrix1);
---
+++ lesson10/Task52/Program.cs
@@ -0,0 +1,70 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел размера NxN.
+// Укажите два числа,означающие строки, которые нужно поменять местами.
+/*
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+9 8 7 2
+число 1 -1
+число 2 -3 
+
+Итог:
+1 2 7 4
+5 3 2 9
+8 4 2 4
+9 2 7 8
+*/
+
+int[,] GetMatrix()
+{
+    Console.Write($"Введите размер двумерного массива (NxN): ");
+    int rows = Convert.ToInt32(Console.ReadLine());
+    int cols = rows;
+    int[,] matrix = new int[rows, cols];
+    Random rand = new Random();
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            matrix[i, j] = rand.Next(-100, 100);
+        }
+    }
+    return matrix;
+}
+
+int[,] ChangeRows(int[,] matrix)
+{
+    Console.Write("Введите число, означающее строку, которую будем заменять на другую: ");
+    int row1 = Convert.ToInt32(Console.ReadLine()) - 1;
+    Console.Write("Введите число, означающее строку, с которой будем менять первую: ");
+    int row2 = Convert.ToInt32(Console.ReadLine()) - 1;
+    if (row1 < matrix.GetLength(0) && row2 < matrix.GetLength(0))
+    {
+        for (int j = 0, k = 0; j < matrix.GetLength(0); j++)
+        {
+            k = matrix[row1, j];
+            matrix[row1, j] = matrix[row2, j];
+            matrix[row2, j] = k;
+        }
+    }
+    else Console.WriteLine("Введите корректные исходные данные");
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]}  ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] array = GetMatrix();
+PrintMatrix(array);
+array = ChangeRows(array);
+PrintMatrix(array);
