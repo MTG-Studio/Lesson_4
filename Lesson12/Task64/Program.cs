5f7b60f43297d79bb3de5b68c27f47083e2d1761
mtgstudio <mtg2006@mail.ru>
2022-08-26 18:06:12 +0500

    Решение задач 54 56 58 60 62

---
+++ Lesson12/Task64/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 64: Задайте значения M и N.
+// Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+// M = 1; N = 5. -> ""1, 2, 3, 4, 5""
+// M = 4; N = 8. -> ""4, 6, 7, 8""
+
+void InputNumbers(out int min, out int max)
+{
+    Console.Write("Введите число M: ");
+    min = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Введите число N: ");
+    max = Convert.ToInt32(Console.ReadLine());
+}
+
+void PrintNaturalNumbers(int min, int max)
+{
+    for (int i = min; i < max; i++)
+    {
+        Console.Write($"{min}, ");
+        min++;
+    }
+    Console.WriteLine(max);
+}
+
+InputNumbers(out int M, out int N);
+PrintNaturalNumbers(M, N);
---
+++ Lesson12/Task66/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 66: Задайте значения M и N.
+// Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+void InputNumbers(out int min, out int max)
+{
+    Console.Write("Введите число M: ");
+    min = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Введите число N: ");
+    max = Convert.ToInt32(Console.ReadLine());
+}
+
+void PrintSum(int min, int max)
+{
+    int sum = 0;
+    for (int i = min; i <= max; i++, min++)
+        sum += min;
+    Console.WriteLine(sum);
+}
+
+InputNumbers(out int M, out int N);
+PrintSum(M, N);
